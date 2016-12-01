213c89eb26665bf2fff8fd80e92977fcdb76c910
Stanislav Gulis <stanislav.gulis@gmail.com>
2016-12-01 22:02:51 +0100

    ReactiveUI-WinForms example

---
+++ ReactiveUiApp/FlickrPhoto.vb
@@ -0,0 +1,10 @@
+﻿
+Public Class FlickrPhoto
+
+    Public Property Title As String
+
+    Public Property Description As String
+
+    Public Property Url As String
+
+End Class
---
+++ ReactiveUiApp/MainWindow.vb
@@ -0,0 +1,17 @@
+﻿
+Public Class MainWindow
+
+    Public Property ViewModel As AppViewModel
+
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        ViewModel = New AppViewModel()
+
+    End Sub
+
+End Class
